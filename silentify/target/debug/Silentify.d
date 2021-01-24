01f384d7c2ef3bbb11a42613b9c5495b13b51556
Sai Arvind <sai.arvindg@gmail.com>
2021-01-24 18:41:52 -0500

    fixing directory structure

--- silentify/target/debug/Silentify.d
+++
@@ -1,1 +0,0 @@
-/Users/saiarvind/Projects/Silentify/silentify/target/debug/silentify: /Users/saiarvind/Projects/Silentify/silentify/src/main.rs
--- silentify/target/debug/deps/Silentify.d
+++
@@ -1,5 +0,0 @@
-/Users/saiarvind/Projects/Silentify/silentify/target/debug/deps/silentify: src/main.rs
-
-/Users/saiarvind/Projects/Silentify/silentify/target/debug/deps/silentify.d: src/main.rs
-
-src/main.rs:
