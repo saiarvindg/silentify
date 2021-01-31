cc5652a47a877d9f599e3c104a547a95a480ce89
Sai Arvind <sai.arvindg@gmail.com>
2021-01-31 00:55:23 -0500

    added dot env create

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("silentify");
+    println!("Loading env vars from .env file...");
+    dotenv().ok();
+
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
 }
