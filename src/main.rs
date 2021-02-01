60d20ae6a4e964074a2bd31f0ef1b49025f58859
Sai Arvind <sai.arvindg@gmail.com>
2021-01-31 21:42:38 -0500

    managed to make a call lul

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 extern crate rspotify;
 
+use rspotify::blocking::client::Spotify;
+use rspotify::blocking::oauth2::SpotifyClientCredentials;
+use rspotify::senum::Country;
+
 fn main() {
-    println!("Starting silentify...");
+    let client_credential = SpotifyClientCredentials::default().build();
+    let access_token = client_credential.get_access_token();
 
+    let spotify = Spotify::default()
+        .client_credentials_manager(client_credential)
+        .access_token(&access_token)
+        .build();
 
+    
+    let birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP";
+    let tracks = spotify.artist_top_tracks(birdy_uri, Country::UnitedStates);
+    println!("{:?}", tracks.unwrap());
 }
