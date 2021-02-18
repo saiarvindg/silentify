8405953904e2a68eada11940efab184f4d4f605d
Sai Arvind <sai.arvindg@gmail.com>
2021-02-17 23:39:33 -0500

    got the looping and delaying mostly working

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use rspotify::model::track::FullTrack;
 use tokio::time::{delay_for, Duration};
 
 mod spotifyclient;
 
-const AD_LENGTH: u64 = 15; // default ad length
+const AD_LENGTH: Duration = Duration::from_secs(30); // default ad length
+
+// placeholder to account for random delays like crossfade, network lag, processing, etc. util i figure out a better way
+const HACKY_DELAY: Duration = Duration::from_secs(2);
 
 #[tokio::main]
 async fn main() {
@@ -11,42 +15,54 @@ async fn main() {
     let (name, email) = spotifyclient::get_current_displayname(spotify).await;
     println!("The current logged in user is {} ({})", name, email);
 
-    let run_result = true;
     loop {
         let curr_track = spotifyclient::get_curr_playing_track(spotify).await;
 
-        match curr_track {
-            Some(playing) => {
-                println!("Some Playing object: {:?}", playing);
-                match playing.item {
-                    Some(full_track) => {
-                        println!("Some track is playing: {:?}", full_track);
-                        println!("The track type is {:?}", full_track._type);
-                    },
-                    // in rspotify library None means the item is an ad (not a track, album, playist, episode, etc...)
-                    None => println!("Could not get information about currently playing track.")
-                    
+        if curr_track.is_some() {
+            let playing = curr_track.unwrap();
+            match playing.item {
+                Some(full_track) => {
+                    let curr_progress = match playing.progress_ms {
+                        Some(p) => Duration::from_millis(p.into()),
+                        None => HACKY_DELAY, // going to assume that if progress result is empty but track info exists, nothing has started playing yet but will play
+                    };
+
+                    print_curr_playing(&full_track, curr_progress);
+
+                    let remaining_duration =
+                        Duration::from_millis(full_track.duration_ms.into()) - curr_progress + HACKY_DELAY;
+
+                    println!(
+                        "Checking again in {} seconds",
+                        Duration::as_secs(&remaining_duration)
+                    );
+                    delay_for(remaining_duration).await;
+                    println!(
+                        "Waited for {} seconds",
+                        Duration::as_secs(&remaining_duration)
+                    );
                 }
-
+                // in rspotify library None means the item is not a track, album, playist, episode, etc... so it must an ad
+                None => {
+                    println!("An AD is playing. Muting for {} seconds.", AD_LENGTH.as_secs());
+                    delay_for(AD_LENGTH).await;
+                    println!("Waited for {} seconds", AD_LENGTH.as_secs());
+                }
             }
-            None => println!("Nothing is currently playing. Exiting...")
-        }
-
-        if !run_result { 
-            println!("Exiting...");
-            break;
         } else {
-            println!("waiting for {} seconds", AD_LENGTH);
-            delay_for(Duration::from_secs(AD_LENGTH)).await;
-            println!("waited for {} seconds", AD_LENGTH);
+            println!("Nothing is currently playing. Exiting...");
+            break;
         }
     }
+}
 
-
-
-    // let curr_playing = spotifyclient::get_curr_playing_track(spotify).await;
-    // match curr_playing {
-    //     Some(p) => println!("Playing: {:?}", p),
-    //     None => println!("Nothing is currently playing."),
-    // };
+fn print_curr_playing(full_track: &FullTrack, progress_ms: Duration) {
+    println!(
+        "Currently playing {} is {} by {} lasting for {} seconds with {} seconds already played.",
+        full_track._type.as_str(),
+        full_track.name,
+        spotifyclient::format_artist_vec(&full_track.artists),
+        Duration::as_secs(&Duration::from_millis(full_track.duration_ms.into())),
+        progress_ms.as_secs()
+    );
 }
--- src/spotifyclient.rs
+++ src/spotifyclient.rs
@@ -1,42 +1,71 @@
-use rspotify::{client::Spotify, model::{playing::Playing, user::PrivateUser}, oauth2::{SpotifyClientCredentials, SpotifyOAuth}};
 use rspotify::util::get_token;
+use rspotify::{
+    client::Spotify,
+    model::{artist::SimplifiedArtist, playing::Playing},
+    oauth2::{SpotifyClientCredentials, SpotifyOAuth},
+};
 
 extern crate rspotify;
-
-
 
 pub async fn build_spotify_instance() -> Spotify {
     println!("Creating spotify instance...");
 
     let mut oauth = SpotifyOAuth::default()
-    .scope("user-read-currently-playing")
-    .build();
+        .scope("user-read-currently-playing")
+        .build();
 
     let token_info = match get_token(&mut oauth).await {
         Some(token_info) => token_info,
-        None => panic!("could get token from spotify")
+        None => panic!("could get token from spotify"),
     };
 
-    let client_credentials = SpotifyClientCredentials::default().token_info(token_info).build();
+    let client_credentials = SpotifyClientCredentials::default()
+        .token_info(token_info)
+        .build();
 
-    return Spotify::default().client_credentials_manager(client_credentials).build();
+    return Spotify::default()
+        .client_credentials_manager(client_credentials)
+        .build();
 }
 
 pub async fn get_current_displayname(spotify: &Spotify) -> (String, String) {
     println!("Fetching currently logged in user...");
-    let user = spotify.me().await.expect("Could not fetch current user. Spotify may not be setup properly");
-    let displayname = user.display_name.unwrap_or(String::from("No display name set"));
+    let user = spotify
+        .me()
+        .await
+        .expect("Could not fetch current user. Spotify may not be setup properly");
+    let displayname = user
+        .display_name
+        .unwrap_or(String::from("No display name set"));
     let email = user.email.unwrap_or(String::from("No email address set"));
     return (displayname, email);
 }
 
-
 pub async fn get_curr_playing_track(spotify: &Spotify) -> Option<Playing> {
     println!("Fetching currently playing track...");
-    return spotify.current_user_playing_track().await.expect("Could not fetch currently playing track.");
+    let curr_playing = spotify
+        .current_user_playing_track()
+        .await
+        .expect("Could not fetch currently playing track.");
+
+    return match curr_playing {
+        Some(p) => {
+            if p.is_playing {
+                return Some(p);
+            }
+            return None;
+        }
+        None => None,
+    };
 }
 
-
-// pub fn calc_track_end_time(total_duration: u32, curr_progress: u32) {
-    
-// }
+pub fn format_artist_vec(artists: &Vec<SimplifiedArtist>) -> String {
+    return artists.iter().fold(String::new(), |acc, a| {
+        if acc.is_empty() {
+            // just for the first iteration, return only the initial empty starting string
+            String::from(&a.name)
+        } else {
+            acc + ", " + &a.name
+        }
+    });
+}
